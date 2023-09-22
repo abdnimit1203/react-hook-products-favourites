4667b6b801bb858bdfd051a7bea248575590b9c6
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-09-23 02:18:35 +0600

    Ratings added

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# React + Vite
+# React hooks params routers
+[Live Server Link : Phones](https://650ded0970ade62acbc63531--resplendent-halva-bd4e95.netlify.app/)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Look at the src file and foldering in "src"
+- Top title change , products added through loader 
+- single product page (add to facourites)
+- Favourites page shows all favourites phones
+- Inside favourite( show all show less button)
--- src/Components/Phones/PhonesCard.jsx
+++ src/Components/Phones/PhonesCard.jsx
@@ -1,6 +1,6 @@
 import Rating from "react-rating";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const PhonesCard = ({ phone }) => {
   const { id, title, brand, rating, images, price } = phone || {};
@@ -25,13 +25,43 @@ const PhonesCard = ({ phone }) => {
           <p className="block font-sans text-sm font-normal py-2 leading-normal text-gray-700 antialiased opacity-75">
             {rating}
           </p>
-
+          <div>
           <Rating
-            name="half-rating-read"
-            defaultValue={2.5}
-            precision={0.5}
-            readOnly
-          />
+              emptySymbol={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1}
+                  stroke="#ffa8af"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                  />
+                </svg>
+              }
+              fullSymbol={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#ffc83d"
+                  
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              }
+              initialRating={rating}
+              readonly
+            />
+          </div>
         </div>
         <div className="p-6 pt-0">
           <Link to={`/phones/${id}`}>
@@ -48,7 +78,7 @@ const PhonesCard = ({ phone }) => {
   );
 };
 PhonesCard.propTypes = {
-    phone: PropTypes.object.isRequired
-}
+  phone: PropTypes.object.isRequired,
+};
 
 export default PhonesCard;
